163f5976d7c3686538ff0faca88ba9ed725f0066
HP <sswong1212@gmail.com>
2022-03-19 13:54:32 +0800

    commit

--- app/src/main/java/com/example/gogoalfitness/fragment/SettingFragment.kt
+++ app/src/main/java/com/example/gogoalfitness/fragment/SettingFragment.kt
@@ -1,11 +1,14 @@
 package com.example.gogoalfitness.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.gogoalfitness.R
+import com.example.gogoalfitness.ReminderActivity
+import com.example.gogoalfitness.databinding.FragmentSettingBinding
 
 
 class SettingFragment : Fragment() {
@@ -13,8 +16,46 @@ class SettingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val bind = FragmentSettingBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+
+        bind.reminderCard.setOnClickListener {
+            val intent = Intent(this@SettingFragment.requireContext(), ReminderActivity::class.java)
+            startActivity(intent)
+        }
+
+        bind.FacebookLink.setOnClickListener{
+            val uri = Uri.parse("https://www.facebook.com/")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        bind.InstaLink.setOnClickListener{
+            val uri = Uri.parse("https://www.instagram.com/")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        bind.TwitterLink.setOnClickListener{
+            val uri = Uri.parse("https://twitter.com/")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        bind.RateUsCard.setOnClickListener{
+            val uri = Uri.parse("https://play.google.com/store/apps/")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        bind.FeedbackCard.setOnClickListener{
+            val uri = Uri.parse("mailto:gogoalFitness@gmail.com")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        return bind.root
     }
 
 }
